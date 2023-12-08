3624a2ab3fd3a95cbe6092568aa32231b117b0c8
bhupendrav16 <bhupendra.v.brudite@gmail.com>
2023-12-08 13:07:08 +0530

    rebase

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
 print("first main")
 print("branch added")
-<<<<<<< HEAD
 
 print("changes by the main")
 
 print("feature2 added")
-
-=======
-print("feature2 added")
->>>>>>> e272143 (updates in test.py)
